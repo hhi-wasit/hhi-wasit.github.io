a16969d19b5e89ed067158e37a59a826c5748c03
Higher Health Institution in Wasit <hhiwasit@gmail.com>
2026-06-30 01:00:43 +0300

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -92,6 +92,8 @@ function renderFooter() {
           <p style="margin-top:1rem"><a href="admin.html" style="color:#9ca3af;font-size:.8rem"><i class="fas fa-lock" style="${mr}:.3rem"></i>بوابة الإدارة</a></p>
         </div>
       </div>
-     <div class="footer-bottom">
-  <p>&copy; ${new Date().getFullYear()} Dr. Ahmed Azeez. All rights reserved.</p>
-</div>
+      <div class="footer-bottom">
+        <p>&copy; ${new Date().getFullYear()} د. أحمد عزيز. جميع الحقوق محفوظة</p>
+      </div>
+    </div>`;
+}
